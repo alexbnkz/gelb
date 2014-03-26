6358e2aa5d5ff6d7db6aa18f98ed98505b352c26
Alex Benincasa <alexbenincasa@ymail.com>
2014-03-26 04:13:09 -0300

    listarExperimento, exclus?o do clControle

--- src/java/Web/Experimentos.java
+++ src/java/Web/Experimentos.java
@@ -1,13 +1,13 @@
 package Web;
 
 import Utils.XMLTransform;
-import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Hashtable;
 import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -30,27 +30,48 @@ public class Experimentos extends HttpServlet {
                 hash.put(en.getKey(), en.getValue());
             }
             
+            if(!hash.containsKey("cmd")){
+                hash.put("cmd", "Experimentos/lst");
+            }
+            
             String page = hash.get("cmd").toString().split("/")[0];
             String cmd = hash.get("cmd").toString().split("/")[1].toUpperCase();
             
+            if(cmd.equals("INS") || cmd.equals("UPD") || cmd.equals("DEL")){
+                salvarExperimento(cmd, hash);
+            }
+            
+            String xml = "<root>";
+            
+            xml += listarExperimento(cmd, hash);
+            
+            xml += "</root>";
+            
             String html;
             XMLTransform transform = new XMLTransform();
-            html = transform.toHtml("D:\\GELB\\web\\xsl\\" + page + ".xsl", "<root></root>");
+            html = transform.toHtml("D:\\GELB\\web\\xsl\\" + page + ".xsl", xml);
 
             out.println(html);
-        
+        } catch (Exception e) {
+            out.println(e.toString());
         } finally {            
             out.close();
         }
     }
     
-    private void salvarExperimento(String cmd, String id_experimento, String nm_experimento, String dt_experimento, String tp_experimento, String de_experimento){ 
+    private void salvarExperimento(String cmd, Hashtable hash){ 
         try {
             Class.forName("com.mysql.jdbc.Driver");
             String connectionUrl = "jdbc:mysql://localhost/ifrj?user=root&password=";
             
             Connection con = DriverManager.getConnection(connectionUrl); 
             
+            String id_experimento = hash.get("id_experimento").toString();
+            String nm_experimento = hash.get("nm_experimento").toString();
+            String dt_experimento = hash.get("dt_experimento").toString();
+            String tp_experimento = hash.get("tp_experimento").toString();
+            String de_experimento = hash.get("de_experimento").toString();
+                    
             if(cmd.equals("INS")){
                 ResultSet result = con.createStatement().executeQuery("SELECT MAX(id_experimento)+1 AS NewCodigo FROM tExperimento;");
                 
@@ -61,24 +82,24 @@ public class Experimentos extends HttpServlet {
                 }
                 
                 String SQL = "INSERT INTO";
-                SQL = SQL + " tExperimento(id_experimento, nm_experimento, dt_experimento, tp_experimento, de_experimento) ";
-                SQL = SQL + " VALUES(" + novoCodigo + ", '" + nm_experimento + "', '" + dt_experimento + "', '" + tp_experimento + "', '" + de_experimento + "');";
+                SQL += " tExperimento(id_experimento, nm_experimento, dt_experimento, tp_experimento, de_experimento) ";
+                SQL += " VALUES(" + novoCodigo + ", '" + nm_experimento + "', '" + dt_experimento + "', '" + tp_experimento + "', '" + de_experimento + "');";
                 
                 con.createStatement().execute(SQL);    
             }
             if(cmd.equals("UPD")){
                 String SQL = " UPDATE tExperimento SET ";
-                SQL = SQL + " nm_experimento='" + nm_experimento + "', ";
-                SQL = SQL + " dt_experimento='" + dt_experimento + "', ";
-                SQL = SQL + " tp_experimento='" + tp_experimento + "', ";
-                SQL = SQL + " de_experimento='" + de_experimento + "' ";
-                SQL = SQL + " WHERE id_experimento=" + id_experimento + "; ";
+                SQL += " nm_experimento='" + nm_experimento + "', ";
+                SQL += " dt_experimento='" + dt_experimento + "', ";
+                SQL += " tp_experimento='" + tp_experimento + "', ";
+                SQL += " de_experimento='" + de_experimento + "' ";
+                SQL += " WHERE id_experimento=" + id_experimento + "; ";
                 
                 con.createStatement().execute(SQL);    
             }
             if(cmd.equals("DEL")){
                 String SQL = " DELETE FROM tExperimento ";
-                SQL = SQL + " WHERE id_experimento=" + id_experimento + "; ";
+                SQL += " WHERE id_experimento=" + id_experimento + "; ";
                 
                 con.createStatement().execute(SQL);    
             }
@@ -86,6 +107,64 @@ public class Experimentos extends HttpServlet {
             System.out.println("SQL Exception: "+ e.toString());
         } catch (ClassNotFoundException cE) {
             System.out.println("Class Not Found Exception: "+ cE.toString());
+        }
+    }
+    
+    private String listarExperimento(String cmd, Hashtable hash){ 
+        String xml = "";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String connectionUrl = "jdbc:mysql://localhost/ifrj?user=root&password=";
+            
+            Connection con = DriverManager.getConnection(connectionUrl); 
+            
+//            String id_experimento = hash.get("id_experimento").toString();
+//            String nm_experimento = hash.get("nm_experimento").toString();
+//            String dt_experimento = hash.get("dt_experimento").toString();
+//            String tp_experimento = hash.get("tp_experimento").toString();
+//            String de_experimento = hash.get("de_experimento").toString();
+                    
+            if(cmd.equals("LST")){
+                String SQL = " SELECT  ";
+                SQL += " id_experimento, nm_experimento, dt_experimento, tp_experimento, de_experimento ";
+                SQL += " FROM tExperimento ";        
+                
+                ResultSet result = con.createStatement().executeQuery(SQL);
+                
+                if(!result.wasNull()){
+                    while(result.next()){
+                        xml += " <experimento ";
+                        xml += " id_experimento = '" + result.getInt("id_experimento") + "' ";
+                        xml += " nm_experimento = '" + result.getString("nm_experimento") + "' ";
+                        xml += " dt_experimento = '" + result.getString("dt_experimento") + "' ";
+                        xml += " tp_experimento = '" + result.getString("tp_experimento") + "' ";
+                        xml += " de_experimento = '" + result.getString("de_experimento") + "' ";
+                        xml += " > </experimento> ";
+                    }
+                }
+            }
+//            if(cmd.equals("UPD")){
+//                String SQL = " UPDATE tExperimento SET ";
+//                SQL = SQL + " nm_experimento='" + nm_experimento + "', ";
+//                SQL = SQL + " dt_experimento='" + dt_experimento + "', ";
+//                SQL = SQL + " tp_experimento='" + tp_experimento + "', ";
+//                SQL = SQL + " de_experimento='" + de_experimento + "' ";
+//                SQL = SQL + " WHERE id_experimento=" + id_experimento + "; ";
+//                
+//                con.createStatement().execute(SQL);    
+//            }
+//            if(cmd.equals("DEL")){
+//                String SQL = " DELETE FROM tExperimento ";
+//                SQL = SQL + " WHERE id_experimento=" + id_experimento + "; ";
+//                
+//                con.createStatement().execute(SQL);    
+//            }
+        } catch (SQLException e) {
+            System.out.println("SQL Exception: "+ e.toString());
+        } catch (ClassNotFoundException cE) {
+            System.out.println("Class Not Found Exception: "+ cE.toString());
+        } finally {
+            return xml;
         }
     }
  
